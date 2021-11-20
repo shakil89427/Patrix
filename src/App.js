dd53e6b5425a5d6547910d974a4c4dc0c58000de
shakil89427 <shakilahmed89427@gmail.com>
2021-11-20 22:38:28 +0600

    trusted done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header/Header";
 import Banner from "./Components/Banner/Banner";
+import Trusted from "./Components/Trusted/Trusted";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Banner></Banner>
+      <Trusted></Trusted>
     </>
   );
 }
---
+++ src/Components/Trusted/Trusted.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Trusted.css";
+import company_1 from "../../images/campanies/campany-1.png";
+import company_2 from "../../images/campanies/campany-2.png";
+import company_3 from "../../images/campanies/campany-3.png";
+import company_4 from "../../images/campanies/campany-4.png";
+import company_5 from "../../images/campanies/campany-5.png";
+import company_6 from "../../images/campanies/campany-6.png";
+
+const Trusted = () => {
+  return (
+    <div>
+      <h4 className="text-center mb-3">Trusted by campanies like</h4>
+      <hr className="hr-1" />
+      <hr className="hr-2" />
+      <Container className="mt-3 mx-auto">
+        <Row>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_1} alt="" />
+          </Col>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_2} alt="" />
+          </Col>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_3} alt="" />
+          </Col>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_4} alt="" />
+          </Col>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_5} alt="" />
+          </Col>
+          <Col className="trusted-img-col text-center" sm={12} md={4} lg={2}>
+            <img className="trusted-img" src={company_6} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Trusted;
